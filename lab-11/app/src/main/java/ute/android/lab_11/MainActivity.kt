6009c8140de65c8ecd1e55b7269f277fc8e78654
holetinnghia <holetinnghia@icloud.com>
2025-12-19 14:26:27 +0700

    fix(lab-11): MainActivity

--- lab-11/app/src/main/java/ute/android/lab_11/MainActivity.kt
+++ lab-11/app/src/main/java/ute/android/lab_11/MainActivity.kt
@@ -1,20 +1,36 @@
 package ute.android.lab_11
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import ute.android.lab_11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: ToDoViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        val adapter = TaskAdapter(viewModel)
+        binding.recyclerViewTasks.adapter = adapter
+
+        viewModel.userTasks.observe(this) { tasks ->
+            adapter.submitList(tasks)
+        }
+
+        viewModel.toastMessage.observe(this) { message ->
+            message?.let {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+                viewModel.clearToast()
+            }
         }
     }
 }
