0638c0acd9f3852d99919693f2170eaef1ba285a
holetinnghia <holetinnghia@icloud.com>
2025-12-19 14:26:07 +0700

    add(lab-11): add User

---
+++ lab-11/app/src/main/java/ute/android/lab_11/User.kt
@@ -0,0 +1,11 @@
+package ute.android.lab_11
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val username: String,
+    val password: String
+)
